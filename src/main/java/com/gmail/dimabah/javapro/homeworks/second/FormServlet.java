94a19190096ba76f511b22b787463ab761cfd4d6
Drona <53328757+Drona1@users.noreply.github.com>
2022-12-28 22:41:34 +0200

    Initial commit

---
+++ src/main/java/com/gmail/dimabah/javapro/homeworks/second/FormServlet.java
@@ -0,0 +1,53 @@
+package com.gmail.dimabah.javapro.homeworks.second;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@WebServlet(urlPatterns = "/")
+public class FormServlet extends HttpServlet {
+    List<List<String>> answersList = new ArrayList<>();
+    List<String> questionsList = new ArrayList<>();
+
+    @Override
+    public void init() {
+        questionsList.add("Which PC operating system do you mostly use?");
+        answersList.add(Arrays.asList("Microsoft Windows", "Mac OS", "UNIX OS", "Linux", "Other"));
+        questionsList.add("Which mobile operating system do you mostly use?");
+        answersList.add(Arrays.asList("Android", "Symbian", "iOS", "BlackBerry OS", "Windows Mobile", "Other"));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        req.setAttribute("questionsList", questionsList);
+        req.setAttribute("answersList", answersList);
+        try {
+            getServletContext().getRequestDispatcher("/form.jsp").forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        List<String> answers = new ArrayList<>();
+        for (int i = 0; i < questionsList.size(); i++) {
+            answers.add(request.getParameter("question" + i));
+            System.out.println(request.getParameter("question" + i));
+        }
+        request.setAttribute("questionsList", questionsList);
+        request.setAttribute("answers", answers);
+        try {
+            getServletContext().getRequestDispatcher("/result.jsp").forward(request, response);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
